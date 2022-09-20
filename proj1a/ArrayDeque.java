c7f4881e5d8cc4f57388da880a369a6c571a642a
acnight <63221708+acnight@users.noreply.github.com>
2022-09-20 20:05:13 +0800

    Update ArrayDeque.java

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,6 +1,7 @@
 public class ArrayDeque<T> {
     private T[] theQueue;
     private int size;
+
     public ArrayDeque() {
         theQueue = (T[]) new Object[8];
         size = 0;
@@ -9,22 +10,12 @@ public class ArrayDeque<T> {
     private int ratio;
 
     public boolean isEmpty() {
-        if (size == 0) {
-            return true;
-        }
-        return false;
+            return size == 0;
     }
+
     public int size() {
         return size;
     }
-    private int minusOne(int index, int mode) { //return the before point' number
-        if (index == mode - 1) {
-            return 0;
-        } else {
-            return index + 1;
-        }
-    }
-
     private void firstResize() {
         T[] a = (T[]) new Object[size * 4];
         System.arraycopy(theQueue, 0, a, 1, size);
@@ -66,27 +57,36 @@ public class ArrayDeque<T> {
         theQueue = a;
     }
     public T removeFirst() {
-        T b = theQueue[0];
-        T[] a = (T[]) new Object[theQueue.length];
-        System.arraycopy(theQueue, 1, a, 0, size - 1);
-        theQueue = a;
-        size -= 1;
-        ratio = size * 100 / (theQueue.length);
-        if (ratio < 25 && ratio != 0) {
-            halveSize();
+        if (size == 0) {
+            return null;
+        } else {
+            T b = theQueue[0];
+            T[] a = (T[]) new Object[theQueue.length];
+            System.arraycopy(theQueue, 1, a, 0, size - 1);
+            theQueue = a;
+            size -= 1;
+            ratio = size * 100 / (theQueue.length);
+            if (size / (double)theQueue.length < 0.25 && ratio >= 16) {
+                halveSize();
+            }
+            return b;
         }
-        return b;
     }
     public T removeLast() {
-        T b = theQueue[size - 1];
-        theQueue[size - 1] = null;
-        size -= 1;
-        ratio = size * 100 / (theQueue.length);
-        if (ratio < 25 && ratio != 0) {
-            halveSize();
+        if (size == 0) {
+            return null;
+        } else {
+            T b = theQueue[size - 1];
+            theQueue[size - 1] = null;
+            size -= 1;
+            ratio = size * 100 / (theQueue.length);
+            if (size / (double)theQueue.length < 0.25 && ratio >= 16) {
+                halveSize();
+            }
+            return b;
         }
-        return b;
     }
+
     public T get(int index) {
         return theQueue[index];
     }
@@ -94,15 +94,19 @@ public class ArrayDeque<T> {
 //    public static void main(String[] args) {
 //        ArrayDeque<Integer> ArrayDeque = new ArrayDeque<>();
 //        ArrayDeque.addFirst(0);
-//        ArrayDeque.get(0);      //==> 0
-//        ArrayDeque.removeLast();     // ==> 0
-//        ArrayDeque.addLast(3);
-//        ArrayDeque.removeFirst();     //==> 3
-//        ArrayDeque.addLast(5);
-//        ArrayDeque.get(0);      //==> 5
-//        ArrayDeque.removeFirst();     //==> 5
 //        ArrayDeque.addLast(8);
-//        ArrayDeque.removeFirst();     //==> 8
-//        ArrayDeque.addLast(10);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(8);
+//        ArrayDeque.addLast(3);
+//        ArrayDeque.removeFirst();
+//        ArrayDeque.addLast(5);
+//        ArrayDeque.removeFirst();
+//        ArrayDeque.removeFirst();
+//        ArrayDeque.removeFirst();
+//        ArrayDeque.removeFirst();
 //    }
 }
